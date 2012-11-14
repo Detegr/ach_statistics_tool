e50277cd7f272343143a5ce7e247abc9fc937a4b
Detegr <detegr@gmail.com>
2012-11-14 15:07:15 +0200

    Simple test for HttpInterface

---
+++ src/test/java/fi/tapiiri/software/HttpDataInterface/HttpInterfaceTest.java
@@ -0,0 +1,31 @@
+package fi.tapiiri.software.HttpDataInterface;
+
+import java.net.MalformedURLException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+import junit.framework.Assert;
+
+public class HttpInterfaceTest
+{
+	@Test
+	public void testGetPlayers()
+	{
+		List<Player> players=null;
+		try
+		{
+			players=HttpInterface.getPlayers("http://muum.org:8080");
+		} catch(MalformedURLException e)
+		{
+			Assert.assertTrue(false);
+		}
+		Iterator<Player> it=players.listIterator();
+		while(it.hasNext())
+		{
+			Player p=it.next();
+			System.out.println(p.getFirstName() + " " + p.getLastName());
+		}
+		Assert.assertNotSame(players.size(), 0);
+	}
+}
